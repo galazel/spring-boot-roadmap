8f8143b5ce6ee45c19b6399990128d57919d2d28
Galagar Glyzel <glyzelgalagar14@gmail.com>
2026-02-12 18:38:48 -0800

    springboot basic part 2

---
+++ even_management/src/main/java/com/event_management/even_management/EvenManagementApplication.java
@@ -0,0 +1,13 @@
+package com.event_management.even_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvenManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvenManagementApplication.class, args);
+	}
+
+}
---
+++ even_management/src/test/java/com/event_management/even_management/EvenManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event_management.even_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvenManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
