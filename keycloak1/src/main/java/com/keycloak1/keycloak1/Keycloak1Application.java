3a27e57df69b6636b2842280724d6359924bc1fb
Galagar Glyzel <glyzelgalagar14@gmail.com>
2026-02-26 05:22:29 -0800

    practice keycloak

---
+++ keycloak1/src/main/java/com/keycloak1/keycloak1/Keycloak1Application.java
@@ -0,0 +1,11 @@
+package com.keycloak1.keycloak1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Keycloak1Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Keycloak1Application.class, args);
+	}
+}
---
+++ keycloak1/src/test/java/com/keycloak1/keycloak1/Keycloak1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keycloak1.keycloak1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Keycloak1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
