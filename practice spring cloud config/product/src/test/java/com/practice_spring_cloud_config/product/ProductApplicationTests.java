0c81642730fb02252841cc7f787a4eda6e23a75e
Galagar Glyzel <glyzelgalagar14@gmail.com>
2026-04-17 02:37:52 -0700

    practice spring cloud config

---
+++ practice spring cloud config/product/src/test/java/com/practice_spring_cloud_config/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice_spring_cloud_config.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ practice spring cloud config/user/src/test/java/com/practice_spring_cloud_config/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice_spring_cloud_config.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
